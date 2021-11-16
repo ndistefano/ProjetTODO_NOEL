36bd2f8214e5f5a48a0468c7d9a6a5677550a9e8
LucRoux78 <93314798+LucRoux78@users.noreply.github.com>
2021-11-16 11:06:19 +0100

    Branch1ereversion

--- TD8-projetLucPageCoursesBAK.js
+++ TD8-projetLucPageCoursesBAK.js
@@ -1,25 +1,26 @@
 // Créer un bouton "close" et l'ajouter à chaque item
-let myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("LI");//
 var i; /* Ne pas changer en let !*/
 for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7"); //\u00D7 signe multiplication
+  var span = document.createElement("SPAN");//
+  var txt = document.createTextNode("\u00D7"); //\u00D7 signe multiplication//
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
+
 // Cliquer sur le bouton close pour faire disparaitre l'élément de liste actuel
-let close = document.getElementsByClassName("close");
+var close = document.getElementsByClassName("close");//
 var i; /* Ne pas changer en let !*/
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    let div = this.parentElement;
+    var div = this.parentElement;//
     div.style.display = "none"; //Efface la ligne quand on clique X
   }
 }
 
 // Ajouter le symbole "checked" quand on clique sur un élément de la liste
-let list = document.querySelector('ul');
+var list = document.querySelector('ul');//
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
@@ -28,27 +29,27 @@ list.addEventListener('click', function(ev) {
 
 // Créer un nouvel élément de la liste quand on clique sur "Ajouter"
 function newElement() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("myInput").value;
-  let t = document.createTextNode(inputValue);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("Vous devez taper quelquechose!");
+    alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
+      var div = this.parentElement;
+      //div.style.display = "none";
     }
   }
 }
